893c2bfb8c8ca212d23a7b066c9066a710f33827
valesteb <estebanfullmusic@gmail.com>
2024-07-01 19:31:15 -0500

    first commit

---
+++ ecommerce.js
@@ -0,0 +1,14 @@
+import 
+
+document.getElementById('index').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
+document.getElementById('catalog').addEventListener('click', function() {
+    window.location.href = 'catalog.html';
+});
+
+document.getElementById('contact').addEventListener('click', function() {
+    window.location.href = 'contact.html';
+});
+ 
