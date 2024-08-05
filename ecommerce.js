b0e48fceec1e7b58c5b727b979de8e4d9c4283cf
Esteban Vallejo <56201388+valesteb@users.noreply.github.com>
2024-08-05 18:53:42 -0500

    Voice control addition

--- ecommerce.js
+++ ecommerce.js
@@ -18,4 +18,31 @@ document.getElementById('catalog').addEventListener('click', function() {
 document.getElementById('contact').addEventListener('click', function() {
     window.location.href = 'contact.html';
 });
- 
+
+
+// voice control 
+const voiceOptions = document.querySelector(".voice");
+const startButton = document.querySelector(".start");
+const cancelButton = document.querySelector(".cancel");
+const pitchInput = document.querySelector(".pitch");
+const rateInput = document.querySelector(".rate");
+const volumeInput = document.querySelector(".volume");
+
+SpeechSynthesis.addEventListener("voiceschanged", () => {
+  const voices = SpeechSynthesis.getVoices();
+  const options = voices.map((voice, index) => {
+    return `<option value"${index}">${voice.name}</option>`;
+  });
+  voice.Options.innerHTML = options.join("");
+});
+
+startButton.addEventListener("click", () => {
+  const text = document.querySelector(".text").value;
+  const message = new SpeechSynthesisUtterance(text);
+  const index = voiceOptions.selectedIndex;
+  message.voice = speechSynthesis.getVoices()[index];
+  message.pitch = pitchInput.value;
+  message.rate = rateInput.value;
+  message.volume = volumeInput.value;
+  SpeechSynthesis.speak(message);
+});
